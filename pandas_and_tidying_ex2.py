6e6f7797116aafee92853f8ed45e4379f039da98
ASPP Student <student@localhost>
2019-09-02 17:51:36 +0200

    solutions to exercise 2

---
+++ pandas_and_tidying_ex2.py
@@ -0,0 +1,7 @@
+# answers to the second exercise
+
+# Setting Strain ID as an index
+mutations.index = mutations['Strain ID']
+
+# Slicing the dataframe and printing it 
+print(mutations.loc[['REL11345'], ['Population', 'Generation', 'Total Mutations']])
